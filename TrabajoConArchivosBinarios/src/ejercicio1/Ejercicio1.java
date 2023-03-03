27ac502b28b9fa07940496a47ad493911dec836d
José Antonio Calero Maqueda <97903853+josantcalmaq@users.noreply.github.com>
2023-03-03 12:56:06 +0100

    Update Ejercicio1.java

--- TrabajoConArchivosBinarios/src/ejercicio1/Ejercicio1.java
+++ TrabajoConArchivosBinarios/src/ejercicio1/Ejercicio1.java
@@ -1,1 +1,23 @@
-
+paackage ej1
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Arrays;
+public class Ejercicio1 implements Serializable {
+	public static void main(String[] args) {
+		try(ObjectInputStream entrada=
+			new ObjectInputStream(new FileInputStream("ficheros\\doubles.dat"))){
+			double[]dobleArray= (double[]) entrada.readObject();
+			for(int i=0; i<dobleArray.length; i++) {
+				System.out.println(dobleArray[i]);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}		
+	}
+}
